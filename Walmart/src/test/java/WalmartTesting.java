0b0754ec412ef45870cc8a6792bc18ca1ca01bde
Tashnuva <zamantashnuva@gmail.com>
2021-11-03 19:12:05 -0400

    first commit

---
+++ Walmart/src/test/java/WalmartTesting.java
@@ -0,0 +1,43 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WalmartTesting {
+
+    public WebDriver driver;
+
+    @Before
+    @Test
+    public void Upset() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\tasnu\\IdeaProjects\\web-automation-framework\\Generic\\drivers\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("https://www.walmart.com/");
+        driver.manage().window().maximize();
+
+
+
+
+
+        driver.close();
+
+
+    }
+
+
+
+
+    @After
+    public void after () {
+        driver.quit();
+    }
+}
+
+
+
+
